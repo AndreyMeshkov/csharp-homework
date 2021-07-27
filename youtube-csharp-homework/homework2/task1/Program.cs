f95aaa75c9e63e7418b0f81dc21509be09653fc0
Andrei Miashkou <meshkow.andrei@gmail.com>
2021-07-27 18:15:59 +0300

    feat: add homework2

---
+++ youtube-csharp-homework/homework2/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Enter the number: ");
+            num = int.Parse(Console.ReadLine());
+            
+            if (num % 2 == 0)
+            {
+                Console.WriteLine(num + " is even  number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is odd  number");
+            }
+            Console.ReadLine();
+        }
+    }
+}
