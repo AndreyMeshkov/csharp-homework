f059eeb308a094a606e67457aeea6de065bfa7b8
Andrei Miashkou <meshkow.andrei@gmail.com>
2021-07-28 13:51:38 +0300

    feat: add homework3

---
+++ youtube-csharp-homework/homework3/task1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            double firstNum = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter operation (+ - / *): ");
+            string operation = Console.ReadLine();
+            Console.WriteLine("Enter the second number: ");
+            double secondNum = double.Parse(Console.ReadLine());
+            if(operation == "+")
+            {
+                Console.WriteLine(firstNum + " " + operation +" "+ secondNum + " = " + (firstNum + secondNum));
+            }
+            else if(operation == "-"){
+                Console.WriteLine(firstNum + " " + operation + " " + secondNum + " = " + (firstNum - secondNum));
+            }
+            else if (operation == "*")
+            {
+                Console.WriteLine(firstNum + " " + operation + " " + secondNum + " = " + (firstNum * secondNum));
+            }
+            else if (operation == "/")
+            {
+                Console.WriteLine(firstNum + " " + operation + " " + secondNum + " = " + (firstNum / secondNum));
+            }
+            else
+            {
+                Console.WriteLine("Invalid operation");
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ youtube-csharp-homework/homework3/task2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number: ");
+            double firstNum = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter operation (+ - / *): ");
+            string operation = Console.ReadLine();
+            Console.WriteLine("Enter the second number: ");
+            double secondNum = double.Parse(Console.ReadLine());
+            switch (operation)
+            {
+                case "+":
+                    Console.WriteLine(firstNum + " " + operation + " " + secondNum + " = " + (firstNum + secondNum));
+                    break;
+                case "-":
+                    Console.WriteLine(firstNum + " " + operation + " " + secondNum + " = " + (firstNum - secondNum));
+                    break;
+                case "*":
+                    Console.WriteLine(firstNum + " " + operation + " " + secondNum + " = " + (firstNum * secondNum));
+                    break;
+                case "/":
+                    Console.WriteLine(firstNum + " " + operation + " " + secondNum + " = " + (firstNum / secondNum));
+                    break;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
