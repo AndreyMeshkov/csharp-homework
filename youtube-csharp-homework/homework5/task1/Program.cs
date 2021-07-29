a8472bfccd8bbd13e7a9bd801987b4d85c2e088d
Andrei Miashkou <meshkow.andrei@gmail.com>
2021-07-29 13:43:14 +0300

    feat: add homework5

---
+++ youtube-csharp-homework/homework5/task1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the width of the triangle: ");
+            int width = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < width; i++)
+            {
+                Console.WriteLine(string.Concat(Enumerable.Repeat("#", i + 1)));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = width -1; i >= 0; i--)
+            {
+                Console.WriteLine(string.Concat(Enumerable.Repeat("#", i + 1)));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < width; i++)
+            {
+                Console.Write(string.Concat(Enumerable.Repeat(" ", width - 1 - i)));
+                Console.WriteLine(string.Concat(Enumerable.Repeat("#", i + 1)));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = width -1; i >= 0; i--)
+            {
+                Console.Write(string.Concat(Enumerable.Repeat(" ", width - 1 - i)));
+                Console.WriteLine(string.Concat(Enumerable.Repeat("#", i + 1)));
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
