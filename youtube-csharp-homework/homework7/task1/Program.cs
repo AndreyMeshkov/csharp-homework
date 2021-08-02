9412d9af27dd01a53d427ee3e089d4a2af2c5159
Andrei Miashkou <meshkow.andrei@gmail.com>
2021-08-02 15:51:03 +0300

    feat: add homework7

---
+++ youtube-csharp-homework/homework7/task1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void PrintString()
+        {
+            Console.Write("Enter character: ");
+            char ch = char.Parse(Console.ReadLine());
+            Console.Write("Enter the number of characters: ");
+            int length = int.Parse(Console.ReadLine());
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write(ch);
+            }
+            Console.ReadLine();
+        }
+        static void Main(string[] args)
+        {
+            PrintString();
+        }
+    }
+}
---
+++ youtube-csharp-homework/homework7/task2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2
+{
+    class Program
+    {
+        static int FindIndex(int[] array, int number)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == number)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+        static void Main(string[] args)
+        {
+            int[] myArray = { 1, 2, 4, 6, 7, 3, 9 };
+            int number = 7;
+            Console.WriteLine(FindIndex(myArray, number));
+            Console.ReadLine();
+        }
+    }
+}
